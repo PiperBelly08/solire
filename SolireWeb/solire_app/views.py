39bfba39940594b3688e9b8f06929236a8bfc728
PiperBelly08 <hawarimunte@gmail.com>
2025-05-16 11:49:27 +0700

    feat display data from model to view

--- SolireWeb/solire_app/views.py
+++ SolireWeb/solire_app/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render
 
-# from .models import Ph, Temperature, Moisture, Color
+from .models import SoilCondition
 
 def index(request):
-    ph_data = 0
-    temperature_data = 0
-    moisture_data = 0
-    color_data = 0
-
     return render(
         request,
         'solire_app/index.html',
         {
-            'ph_data': ph_data,
-            'temperature_data': temperature_data,
-            'moisture_data': moisture_data,
-            'color_data': color_data,
+            'soil_conditions': SoilCondition.objects.all(),
         },
     )
