d7f2b61488fc5d74cd254bd3866c37a6a5f078b5
PiperBelly08 <hawarimunte@gmail.com>
2025-06-09 23:55:40 +0700

    feat: display data from MQTT broker

--- SolireWeb/solire_app/models.py
+++ SolireWeb/solire_app/models.py
@@ -6,8 +6,9 @@ class SoilCondition(models.Model):
     ph_value = models.FloatField()
     temperature_value = models.IntegerField()
     moisture_value = models.IntegerField()
-    rgb_value = models.IntegerField()
+    # rgb_value = models.IntegerField()
     timestamps = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.id + self.ph_value + self.temperature_value + self.moisture_value + self.rgb_value)
+        # return str(self.id + self.ph_value + self.temperature_value + self.moisture_value + self.rgb_value)
+        return str(self.id + self.ph_value + self.temperature_value + self.moisture_value)
--- SolireWeb/solire_app/urls.py
+++ SolireWeb/solire_app/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'solire'
+app_name = 'solire_app'
 urlpatterns = [
     path("", views.index, name="index"),
-
+    path("api/insert/", views.insert_data, name="insert_data"),
+    path("api/", views.list_data, name="list_data"),
 ]
