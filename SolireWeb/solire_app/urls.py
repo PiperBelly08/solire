1b06550d6d17a0990e99382bfb1ae0c8a889ede5
PiperBelly08 <hawarimunte@gmail.com>
2025-07-05 02:34:25 +0700

    feat: add generate_report url

--- SolireWeb/solire_app/urls.py
+++ SolireWeb/solire_app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("api/insert/", views.insert_data, name="insert_data"),
     path("api/", views.list_data, name="list_data"),
     path("api/clear/", views.clear_data, name="clear_data"),
+    path("api/report/", views.generate_report, name="generate_report"),
 ]
